0bf26bdd5bf0ac7fccf999a527bdff9c57017607
duaraghav8@gmail <duaraghav8@gmail.com>
2016-07-23 13:45:39 +0530

    Fixed stopTraversal () bug

--- examples/enter-leave.js
+++ examples/enter-leave.js
@@ -6,9 +6,17 @@ let sp = require ('solidity-parser'),
 	code = require ('fs').readFileSync ('./sample.sol').toString (),
 	ast = sp.parse (code);
 
+//console.log (JSON.stringify (ast, null, 2));
+
 solExplore.traverse (ast, {
 	enter: function (node) {
 		console.log ('Exploring ', node.type, ' while going down!');
+		if (node.type === 'StructDeclaration') {
+			//this.skipNodesBelow ();
+			this.stopTraversal ();
+			//return solExplore.traversalOptions.STOP_TRAVERSAL;
+			//return solExplore.traversalOptions.SKIP_NODES_BELOW;
+		}
 	},
 	leave: function (node) {
 		console.log ('Exploring ', node.type, ' while going back up!');
